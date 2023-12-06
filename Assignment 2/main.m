0916f2699a46f204be137bebbfa1b4bbbe7599cd
RenzoKnol-web <152622532+RenzoKnol-web@users.noreply.github.com>
2023-12-06 12:39:05 +0100

    Update main.m

--- Assignment 2/main.m
+++ Assignment 2/main.m
@@ -14,15 +14,15 @@ clc;
 f=0;
 
 %% Question 1 (a)
-par = 2000;
-r=0.026;
-yield =[5.34 4.12 3.72 3.7 3.86 3.92];
+par = 1000;
+r= 0.073;
+yield =[5.34 4.12 3.72 3.7 3.86 3.92 3.95 3.97];
 T= length(yield)
 
 % Price the bond
 coupon= r*par;
 cash_flow=coupon+zeros(1,T)
-cash_flow(6)=cash_flow(6)+par
+cash_flow(8)=cash_flow(8)+par
 P= sum(cash_flow./((1+yield./100).^(1:T)))
 
 %% Question 1 (b)
@@ -50,3 +50,23 @@ daspect([1 1.5 1]);
 ylim([3 10])
 axis tight; 
 hold off
+
+%% Question 2 (a)
+% Load the data using hist_stock_data.m
+price = hist_stock_data('01012012',['31122022'],'INTC');
+% Calculating simple returns
+simple_y=(price.Close(2:end)-price.Close(1:end-1))./price.Close(1:end-1)
+% Obtain sample statistics for simple returns
+mean(simple_y)
+std(simple_y)
+min(simple_y)
+max(simple_y)
+% Obtain first and third quartiles for simple returns
+% Step 1 - rank the data
+y = sort(simple_y);
+% Compute first quartile
+Q(1) = median(y(find(y<median(y))));
+display (Q(1))
+% Compute third quartile
+Q(3) = median(y(find(y>median(y))));
+display (Q(3))
