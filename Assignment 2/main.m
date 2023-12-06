a19283b334f7529e4b289207b7c43df7f86cb1e0
RenzoKnol-web <152622532+RenzoKnol-web@users.noreply.github.com>
2023-12-06 08:43:56 +0100

    Update main.m

---
+++ Assignment 2/main.m
@@ -0,0 +1,14 @@
+%% Stock Data Intel Corporation (2012-2022)
+% For Empirical Finance 3.2
+% Assignment 2
+% By team 40 (Levi Szabo, Barry Nicolas en Renzo Vermeulen)
+
+%% House Keeping 
+
+% Housekeeping
+clear all;
+close all;
+clc; 
+
+% Figure counter
+f=0;
